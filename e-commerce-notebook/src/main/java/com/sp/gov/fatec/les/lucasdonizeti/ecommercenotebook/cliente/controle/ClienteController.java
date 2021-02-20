fd13c74a6ac669fa239118f52fd5a43c08910552
Lucas Donizeti <lucasdonizete11@gmail.com>
2021-02-20 20:36:00 -0300

    (wip) paginas web

--- e-commerce-notebook/src/main/java/com/sp/gov/fatec/les/lucasdonizeti/ecommercenotebook/cliente/controle/ClienteController.java
+++ e-commerce-notebook/src/main/java/com/sp/gov/fatec/les/lucasdonizeti/ecommercenotebook/cliente/controle/ClienteController.java
@@ -1,25 +1,12 @@
 package com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.cliente.controle;
 
 
-import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.cliente.Cliente;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.cliente.TipoCliente;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.cliente.dto.ClienteDTO;
 import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.cliente.servico.ClienteServico;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.documento.Documento;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.documento.TipoDocumento;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.endereco.Cidade;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.endereco.Endereco;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.endereco.Estado;
-import com.sp.gov.fatec.les.lucasdonizeti.ecommercenotebook.endereco.TipoEndereco;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
 /**
  * author LucasDonizeti
